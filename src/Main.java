159c220efb20be46a093d9794af92994153fefbe
Angelo Netho <angelonetho@gmail.com>
2025-10-02 14:48:41 -0300

    Add main menu to the application with basic input handling

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,57 @@
-package PACKAGE_NAME;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+        exhibitMenu();
+    }
+
+    private static void exhibitMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        int selectedOption = -1;
+
+        while (selectedOption != 0) {
+            System.out.println("Selecione uma operação:");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Codificar");
+            System.out.println("3 - Decodificar");
+            System.out.println("0 - Sair");
+
+            try {
+                selectedOption = scanner.nextInt();
+
+                switch (selectedOption) {
+                    case 1:
+                        // Inserir
+
+                        break;
+                    case 2:
+                        // Codificar
+
+                        break;
+                    case 3:
+                        // Decodificar
+
+                        break;
+                    case 0:
+                        System.out.println("bye.");
+
+                        break;
+                    default:
+                        System.out.println("Operação inválida. Tente novamente.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Input inválido. Tente novamente.");
+
+                scanner.nextLine(); // Limpa o buffer do scanner
+                selectedOption = -1;
+            }
+
+        }
+        scanner.close();
+
+    }
+
 }
