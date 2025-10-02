3bcea156c8c6aa565f12ae257bc73bbe1d82fdde
Angelo Netho <angelonetho@gmail.com>
2025-10-02 17:07:58 -0300

    Add Node class to represent tree nodes in BST

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+public class Node {
+    char letter;
+    Node left, right;
+
+    public Node(char letter) {
+        this.letter = letter;
+        this.left = null;
+        this.right = null;
+    }
+}
